8936fefeb60cb85eac858b9330d58119f0513518
Gianluigi De Marco <dmgiangi@gmail.com>
2022-08-02 11:50:01 +0200

    add datasource

--- src/main/java/com/github/terravivaproject/terraviva/TerraVivaApplication.java
+++ src/main/java/com/github/terravivaproject/terraviva/TerraVivaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TerraVivaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TerraVivaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TerraVivaApplication.class, args);
+    }
 
 }
