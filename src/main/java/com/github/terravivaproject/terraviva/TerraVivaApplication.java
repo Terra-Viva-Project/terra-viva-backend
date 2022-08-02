75c8135c563ccb6687d5b8c41351ca9a71a716ed
Gianluigi De Marco <dmgiangi@gmail.com>
2022-08-02 10:36:08 +0200

    initial spring boot empty project

---
+++ src/main/java/com/github/terravivaproject/terraviva/TerraVivaApplication.java
@@ -0,0 +1,13 @@
+package com.github.terravivaproject.terraviva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraVivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraVivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/terravivaproject/terraviva/TerraVivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.terravivaproject.terraviva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraVivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
