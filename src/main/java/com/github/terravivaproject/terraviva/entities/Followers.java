06b33d0aa96c01a010354cdf27f5c1171e6a6977
Salvatore De Gregorio <salvatore.degregorio03@gmail.com>
2022-08-29 16:13:51 +0200

    Update

--- src/main/java/com/github/terravivaproject/terraviva/entities/Followers.java
+++ src/main/java/com/github/terravivaproject/terraviva/entities/Followers.java
@@ -1,2 +1,30 @@
-package com.github.terravivaproject.terraviva.entities;public class Followers {
+package com.github.terravivaproject.terraviva.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Followers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long follower_id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long followed_id;
+    @Column(nullable = false)
+    private LocalDateTime follow_datetime;
+
+    @JoinColumn
+    @OneToMany(mappedBy = User)
+    private List<User> user_followers;
 }
--- src/main/java/com/github/terravivaproject/terraviva/entities/Tag.java
+++ src/main/java/com/github/terravivaproject/terraviva/entities/Tag.java
@@ -1,2 +1,35 @@
-package com.github.terravivaproject.terraviva.entities;public class tag {
+package com.github.terravivaproject.terraviva.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.eclipse.jetty.server.Authentication;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(nullable = false)
+    private String tag;
+
+    @JoinTable(name = "tag_followers")
+    @JoinColumn
+    @ManyToMany (mappedBy = User)
+    private List<User> tag_followers;
+
+    @JoinTable(name = "rt_posts_tags")
+    @JoinColumn
+    @ManyToMany(mappedBy = Post)
+    private List<Post> rt_posts_tags;
+
 }
