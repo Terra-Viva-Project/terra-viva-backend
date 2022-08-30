ead0bf9aa1c549cbdf469ddd4cc4aa5cc0ca68f7
xpulvi <pulvirenti.n.a@gmail.com>
2022-08-30 10:00:55 +0200

    add Entity: Species, Varieties

---
+++ src/main/java/com/github/terravivaproject/terraviva/entities/Varieties.java
@@ -0,0 +1,45 @@
+package com.github.terravivaproject.terraviva.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Varieties {
+
+    @Id
+    @NotBlank
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank
+    @NotNull
+    private String name;
+
+
+    @OneToOne
+    private Species species;
+
+    //TODO implements @Pattern -> URI
+    private String variety_image;
+
+    @NotBlank @NotNull
+    private LocalDateTime dateTime = LocalDateTime.now();
+
+
+}
